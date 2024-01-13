5901bad477b6aa732d61d3063bb1c903e83279b2
shirish-09 <shirishmainali2@gmail.com>
2024-01-13 22:23:46 +0545

    API and use of then

---
+++ cypress/e2e/CRS-V2/API.cy.js
@@ -0,0 +1,45 @@
+it('Using then', () => {
+    
+    cy.visit('https://www.demoblaze.com/index.html')
+    // cy.get('#login2').click()
+    // cy.get('#loginusername').type('Shirish Mainalii')
+    // cy.get('#loginpassword').type('Shirish@1234')
+
+    cy.get('.btn-primary').eq(2).then((letscheck)=>{
+        expect(letscheck).to.have.text('Log in')
+        expect(letscheck).exist
+        expect(letscheck.text().length).to.be.eq(6)
+    })
+    
+});
+
+
+
+
+
+it('GET', () => {
+    cy.request({
+        method : "GET",
+        url : "https://jsonplaceholder.typicode.com/comments"
+
+    }).then((Response)=>{
+        expect(Response.status).to.eq(200)
+        expect(Response.body[3].id).to.eq(4)
+        expect(Response.body[13].name).to.eq('et officiis id praesentium hic aut ipsa dolorem repudiandae')
+
+    })
+
+});
+
+it('GET', () => {
+    
+    cy.request({
+        method : "GET",
+        url : 'https://api.demoblaze.com/entries'
+
+    }).then((response)=>{
+        expect(response.status).to.eq(200)
+        expect(response.body.Items[0].cat).to.eq("phone")
+        expect(response.body.Items[5].price).to.eq(320)
+    })
+});
---
+++ cypress/e2e/CRS-V2/login.cy.js
@@ -0,0 +1,147 @@
+// ///<reference types = "cypress"/>
+
+
+// // describe('Login', () => {
+
+// //     beforeEach(() => {
+// //         cy.visit('/')
+        
+// //     });
+
+// //     it('Check when valid credentials is provided', () => {
+        
+// //         cy.url().should('include','/login-client')
+        
+// //         cy.location('href').should('include','/login-client')
+// //         //Finding child div
+// //         // cy.get('.w-full').find('.text-sm').should('exist')
+
+// //         //Logo
+// //         cy.get('.object-contain').should('exist')
+       
+// //         cy.get('.text-sm').eq(2).type('012',{delay:700})
+// //         cy.get('.text-sm').eq(4).type('prakriti@yopmail.com')
+// //         cy.get('.text-sm').eq(6).type('Test@123',{delay:50})
+// //         cy.get('.right-3').eq(2).click()
+// //         cy.get('.w-full').eq(8).click()
+// //         cy.get('.text-start ').eq(0).should('exist').and('have.text','Comprehensive Reconciliation System',{delay:500})
+
+// //         cy.go('back',{delay:500})
+// //        // cy.pause()
+// //         cy.go('forward')
+        
+       
+        
+// //    });
+
+// //     it('Check when invalid Client Code is provided', () => {
+
+// //         cy.get('.text-sm').eq(2).type('01',{delay:700})
+// //         cy.get('.w-full').eq(8).click()
+// //         //Capturing toast message 
+// //         cy.get('.text-start').should('be.visible')
+// //         cy.get('.text-start').should('contain','Invalid Client')
+    
+// //     });
+
+// //     it('Check when invalid username and password is provided', () => {
+
+// //         cy.get('.text-sm').eq(2).type('012',{delay:700})
+// //         //Using Alias
+// //         cy.get('[name="username"]').as('User Name')
+// //         cy.get('@User Name').type("abcd@yopmail.com")
+// //         cy.get('.text-sm').eq(6).type('Test@123',{delay:50})
+// //         cy.get('.w-full').eq(8).click()
+// //         cy.get('.text-start').eq(1).should("be.visible")
+// //         cy.get('.text-start').eq(1).should('contain','Invalid username or password')
+// //     });
+    
+
+// //     afterEach(() => {
+
+// // //Logout
+// //        cy.get('.text-start').eq(3).click()
+// //        cy.get('.text-start').eq(8).click()
+       
+
+// //     });
+// // });
+
+// //------------------------------------- Third class----------------------------------------------------------
+
+// // const crsURl = "192.168.50.161/login-client"
+// // const clientcode = Cypress.env('clientcode')
+// // const username = Cypress.env('username')
+// // const password = Cypress.env('password')
+// //     it('const,network interceptor(json),within,dropdown,environment,photo-attach', () => {
+
+       
+       
+//        //pre login network interceptor (json)
+//         cy.intercept('GET','http://192.168.50.161/login-client').as('prelogin')
+//         // cy.visit(Cypress.env('staging_url'))
+       
+//        //using const
+//         cy.visit(crsURl)
+
+       
+//         cy.get('[name= "clientCode"]').type(clientcode)
+//         cy.get('.right-3').eq(2).click()
+//         cy.get('[name= "username"]').type(username)
+//         cy.get('[name= "password"]').type(password)
+//         cy.get('.w-full').eq(8).click()
+//         cy.wait('@prelogin')
+
+//         cy.intercept('GET','http://192.168.50.161:8070/communication/api/v1/notifications/get-all').as('postlogin')
+//         cy.wait('@postlogin')
+
+// //dropdown
+//         cy.get('.text-start').eq(5).click()
+//         cy.get('#react-select-2-option-0').eq(1).click()
+
+
+    
+//          cy.get('.mt-6').eq(0).within(()=>{
+
+//             cy.get('.px-3').should('exist')
+//             cy.get('.w-full').should('exist')
+           
+//      })
+   
+
+// //file upload
+//         cy.visit("https://the-internet.herokuapp.com/upload")
+//         cy.get("#file-upload").selectFile("cypress/fixtures/test.avif")
+//       //for drag and drop  (add npm install --save-dev cypress-file-upload and import 'cypress-file-upload' in command.js;)
+//         //drag and drop
+        
+//             cy.visit("https://the-internet.herokuapp.com/upload")
+//             cy.get("#drag-drop-upload").attachFile("/test.avif", { subjectType: 'drag-n-drop' })
+
+//             //attach file
+            
+//                 cy.visit("https://the-internet.herokuapp.com/upload")
+//                 cy.get("#file-upload").attachFile("/test.avif") 
+                
+                
+           
+
+
+    // })
+    // it.only('demo', () => {
+    //   cy.visit(Cypress.env('stat'))
+    // });
+    it('json.evn', () => {
+      cy.visit(Cypress.env('live_url'));
+      });
+
+
+      
+    
+
+       
+
+
+    
+
+  
