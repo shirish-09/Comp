a26b92c06ba67f714693138173c97d54ae931bfb
shirish-09 <shirishmainali2@gmail.com>
2024-01-31 22:15:21 +0545

    Fifth CLass

--- cypress/e2e/CRS-V2/API.cy.js
+++ cypress/e2e/CRS-V2/API.cy.js
@@ -1,3 +1,5 @@
+let pricetage = "price"
+
 it('Using then', () => {
     
     cy.visit('https://www.demoblaze.com/index.html')
@@ -14,7 +16,7 @@ it('Using then', () => {
 });
 
 
-it.only('GET', () => {
+it('GET', () => {
     cy.request({
         method : "GET",
         url : "https://jsonplaceholder.typicode.com/comments"
@@ -28,7 +30,7 @@ it.only('GET', () => {
 
 });
 
-it.only('GET', () => {
+it('GET', () => {
     
     cy.request({
         method : "GET",
@@ -38,5 +40,41 @@ it.only('GET', () => {
         expect(response.status).to.eq(200)
         expect(response.body.Items[0].cat).to.eq("phone")
         expect(response.body.Items[5].price).to.eq(320)
+    })
+});
+/////////////////////////////Fifth Class////////////
+it('post ', () => {
+    cy.request({
+        method: "POST",
+        url: "https://api.demoblaze.com/login",
+        body: {
+            username : "Shirish Mainali",
+            password : "$hirish@1234"
+        }
+    
+
+    }).then((response) => {
+        expect(response.status).to.eq(200)
+    })
+});
+
+it('get after post ', () => {
+    cy.request({
+        method : "GET",
+        url : "https://api.demoblaze.com/entries"
+    }).then((response) =>{
+       Cypress.env('price',response.body.Items.price)
+       const pricetag = Cypress.env('price')
+    })
+});
+
+
+it.only(' get  ', () => {
+    const pricetag = Cypress.env('price')
+    cy.request({
+        method : "GET",
+        url : "https://api.demoblaze.com/entries"
+    }).then((response) => {
+        expect(response.body.Items[1].price).to.eq(820);
     })
 });
